ca4f487289fcc0433a43c3b06836f1b1b63f08eb
Hannah Kaplan <hhkaplan1@gmail.com>
2017-03-30 13:29:24 -0400

    Update ContRem

---
+++ ConcatenateFTIRoutputs.R
@@ -0,0 +1,32 @@
+#Define a file path for example to run
+#fpath <- "/Users/hannahkaplan/Downloads/CaF2_KnollSampleError_GG1_3_9_14"
+
+FTIR.output.to.matrix <- function(fpath) {
+  
+  #import all CSV files from folder listed in path and add to list
+  filenames <- list.files(fpath, pattern="*.CSV", full.names=TRUE)
+  ldf <- lapply(filenames, read.csv)
+  
+  #combine all data into one matrix taking wavenumber info from the first file
+  dataMatrix <- ldf[[1]]
+  for (i in 2:length(ldf)) {
+    
+    add <- ldf[[i]]
+    dataMatrix <- cbind(dataMatrix, add[2])
+    
+  }
+  
+  #Divide reflectance data by 100 to get fraction relative to 1
+  dataMatrix[, 2:ncol(dataMatrix)] <- dataMatrix[, 2:ncol(dataMatrix)]/100
+  
+  #Insert a row of containing wavelength (um and nm)
+  dataMatrix <- cbind((10000/dataMatrix[,1]), dataMatrix[, 2:ncol(dataMatrix)])
+  
+  #Sort data by ascending wavelength
+  ind <- order(-dataMatrix[,1])
+  dataMatrix <- dataMatrix[ind,]
+  return(dataMatrix)
+  
+  #Plot results
+  plot(dataMatrix[,2], dataMatrix[,4])
+}
---
+++ ConvexHullContinuumRemoval.R
@@ -0,0 +1,45 @@
+# inputData <- dataMatrix 
+# lwav <- 3.1
+# rwav <- 3.8
+
+ConvexHullContinuumRemoval <- function(inputData,lwav,rwav){
+  #require the necessary packages to run the function
+  require("grDevices")
+  
+  #subset the data based on the wavelength positions given
+  ind1 <- min(which(inputData[,1]>lwav))
+  ind2 <- min(which(inputData[,1]>rwav))
+  subData <- inputData[ind1:ind2,]
+  
+  #pre allocate space
+  cont_rem <- matrix(0, dim(subData)[1], dim(subData)[2]-1)
+  cont_fit <- matrix(0, dim(subData)[1], dim(subData)[2]-1)
+  
+  #Run through each data point
+  for (i in 2:dim(subData)[2]){
+    
+    #Fit a convex hull to the reflectance columns
+    cpoints <- chull(subData[,c(1,i)])
+    pos <- which(diff(cpoints)>0) #remove the bottom points to get only upper hull
+    upperhull <- cpoints[pos]
+    upperhull_full <- approx(subData[upperhull,1], subData[upperhull,i], subData[,1], method = "linear")
+    cont_fit[,i-1] <- upperhull_full$y 
+    
+    #Divide out the continuum
+    cont_rem[,i-1] <- subData[,i] / cont_fit[,i-1]
+    
+  }
+  
+  #finalize matrices to include wavelengths
+  cont_rem <- cbind(subData[,1],cont_rem)
+  cont_fit <- cbind(subData[,1],cont_fit)
+  #return both cont_rem and cont_fits labeled in a list
+  return(list(cont_rem = cont_rem,cont_fit = cont_fit))
+  
+  #Plot the results
+  par(mfrow=c(2,1))
+  plot(subData[,1], subData[,2], col = "black")
+  lines(cont_fit[,1], cont_fit[,2], col = "red")
+  plot(subData[,1], subData[,3], col = "black")
+  lines(cont_fit[,1], cont_fit[,3], col = "red")
+}
